9b2a77250edd7f7a2941059147fd4effa9048a54
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-07-04 23:50:59 +0300

    refactor udp writer

--- networking/udp.go
+++ networking/udp.go
@@ -7,16 +7,19 @@ import (
 // Writer is a simple implementation of the writer interface, for sending
 // UDP messages
 type Writer struct {
-	address    *net.UDPAddr
-	connection *net.UDPConn
+	addr *net.UDPAddr
+	conn *net.UDPConn
 }
 
 // NewUDPWriter returns a new UDP Writer
 func NewUDPWriter(givenConnection *net.UDPConn, givenAddress *net.UDPAddr) *Writer {
-	return &Writer{address: givenAddress, connection: givenConnection}
+	return &Writer{
+		addr: givenAddress,
+		conn: givenConnection,
+	}
 }
 
 // Write is an implementation of the standard io.Writer interface
-func (udpWriter *Writer) Write(givenBytes []byte) (int, error) {
-	return udpWriter.connection.WriteToUDP(givenBytes, udpWriter.address)
+func (w *Writer) Write(givenBytes []byte) (int, error) {
+	return w.conn.WriteToUDP(givenBytes, w.addr)
 }
