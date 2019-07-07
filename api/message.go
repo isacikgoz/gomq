fabc2bafc6378caf631d7d8c4b5bfbfaa756f8af
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-07-07 14:17:28 +0300

    add sender to the api

--- api/message.go
+++ api/message.go
@@ -4,6 +4,7 @@ import "encoding/json"
 
 // AnnotatedMessage consists of the bare message and headers
 type AnnotatedMessage struct {
+	Sender  string          `json:"sender"`  // sender name
 	Command string          `json:"command"` // pub/sub
 	Target  string          `json:"target"`  // defines the topic/or queue name
 	Payload json.RawMessage `json:"Payload"` // the original message
